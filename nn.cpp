e8c717c7a0c405ad3f9ea89b5b206ba657f583f2
linkelvin11 <lin.kelvin11@gmail.com>
2015-12-06 02:40:46 -0500

    need to debug training function

--- nn.cpp
+++ nn.cpp
@@ -1,5 +1,6 @@
 #include "nn.h"
 
+// define logistic sigmoid and its derivative
 #define SIG(x) 1/(1+exp(-x))
 #define SIGD(x) SIG(x)*(1-SIG(x))
 
@@ -17,7 +18,6 @@ bool NeuralNet::load(string infile){
     N_h = stoi(instring);
     filein >> instring;
     N_o = stoi(instring);
-    cout << N_i << N_h << N_o << endl;
 
     //resize weights array accounting for bias
     weights.resize(2);
@@ -35,19 +35,16 @@ bool NeuralNet::load(string infile){
         for (int i = 0;i < N_i+1; i++){
             filein >> instring;
             weights[0][i][h] = stod(instring);
-            cout << weights[0][i][h] << ' ';
         }
-        cout << endl;
     }
     for (int o = 0; o < N_o; o++){
         for (int h = 0; h < N_h+1; h++){
             filein >> instring;
             weights[1][h][o] = stod(instring);
-            cout << weights[1][h][o]<< ' ';
         }
-        cout << endl;
     }
 
+    return true;
 }
 
 bool NeuralNet::save(string outfile){
@@ -73,11 +70,141 @@ bool NeuralNet::save(string outfile){
         fileout << endl;
     }
 
+    return true;
+}
+
+void NeuralNet::train(Dataset &data, double learnRate, int numEpochs){
+    // weights are assumed to have been initalized when the NeuralNet is loaded
+
+    if (N_i != data.getN_i() || N_o != data.getN_o()){
+        cerr << "ERROR: Dataset and Network size mismatch\n";
+        exit(1);
+    }
+
+    // initialize a few variables to help with bookkeeping
+    vector< vector<double> > activations(3,vector<double>(1,0));
+    activations[0].resize(N_i+1);
+    activations[1].resize(N_h+1);
+    activations[2].resize(N_o);
+
+    vector< vector<double> > layerSum(activations);
+    vector< vector<double> > deltas(activations);
+    double tmp;
+
+
+    int numSamples = data.getN_s();
+    for(int epoch = 0; epoch < numEpochs; epoch++){
+        for(int s = 0; s < numSamples; s++){
+
+            cout << "initialize input layer for sample " << s << endl;
+            // for each node i in the input layer get the activation
+            for(int i = 0; i < N_i; i++){
+                activations[0][i] = data.getFeature(s,i);
+            }
+            //activations[0][N_i] = 1;
+
+            cout << "forward propagate the activations\n";
+            // for the non-input layers, forward propagate the activations
+            for(int l = 1; l < 3; l++){
+                // for each node j in the layer, update the activation from the previous layer
+                for(int j = 0; j < activations[l].size(); j++){
+                    //layerSum[l][j] = 0;
+                    // layerSum[l][j] = weights[l-1][j][weights[l-1][j].size()-1];
+                    // for each node k in the previous layer, add the weighted activation to the current node
+                    for(int k = 0; k < activations[l-1].size(); k++){
+                        layerSum[l][j] += weights[l-1][k][j] * activations[l-1][k];
+                    }
+                    activations[l][j] = SIG(layerSum[l][j]);
+                }
+                // if (l < 2)
+                //     activations[l][activations[l].size()-1] = 1;
+            }
+
+            cout << "calculate deltas for output layer\n";
+            // propagate deltas backward from output layer
+            // for each node o in the output layer, get the delta
+            for(int o = 0; o < N_o; o++){
+                deltas[2][o] = SIGD(layerSum[2][o]) * (data.getLabel(s,o) - activations[2][o]);
+            }
+
+            cout << "back propagate the deltas from the output layer\n";
+            // go back through the layers and calculate delta for each node
+            // only 1 hidden layer, so no looping through layers. Just update hidden layer
+            // for each node h in layer update the delta
+            tmp = 0;
+            for (int h = 0; h < deltas[1].size()-1; h++){
+                // loop through next layer to get sum of the deltas * weights
+                for (int o = 0; o < deltas[2].size(); o++){
+                    tmp += weights[1][h][o] * deltas[2][o];
+                }
+                deltas[1][h] = SIGD(layerSum[1][h]) * tmp;
+            }
+
+            cout << "update each weight\n";
+            // loop through each weight in the network and apply the update equation
+            for(int l = 0; l < weights.size(); l++){
+                for(int i = 0; i < weights[l].size(); i++){
+                    for(int j = 0; j < weights[l][i].size(); j++){
+                        weights[l][i][j] += deltas[l+1][j] * activations[l][i] * learnRate;
+                    }
+                }
+            }
+
+        }
+ 
+    }
+
+    this->save("trainedoutput.txt");
+
+
+}
+
+// load a training set
+bool Dataset::load(string infile){
+    ifstream filein;
+    filein.open(infile);
+    string instring;
+
+    // read dataset params
+    filein >> instring;
+    N_s = stoi(instring);
+    filein >> instring;
+    N_i = stoi(instring);
+    filein >> instring;
+    N_o = stoi(instring);
+
+    // resize vectors
+    features.resize(N_s);
+    labels.resize(N_s);
+    for(int s = 0; s < N_s; s++){
+        features[s].resize(N_i);
+        labels[s].resize(N_o);
+    }
+
+    // populate data vectors
+    for(int s = 0; s < N_s; s++){
+        for (int i = 0; i < N_i; i++){
+            filein >> instring;
+            features[s][i] = stod(instring);
+        }
+        for(int o = 0; o < N_o; o++){
+            filein >> instring;
+            labels[s][o] = stod(instring);
+        }
+    }
+}
+
+// save a testing set
+bool Dataset::save(string outfile){
+
 }
 
 int main(){
     NeuralNet a;
-    a.load(string("test.txt"));
-    a.save(string("save.txt"));
+    a.load(string("wdbc.init"));
+    //a.save(string("save.txt"));
+    Dataset d;
+    d.load("wdbc_mini.train");
+    a.train(d,0.1,1);
     return 0;
 }
--- nn.h
+++ nn.h
@@ -1,12 +1,31 @@
 #include "constants.h"
 
+class Dataset {
+    int N_s, N_i, N_o;
+    std::vector< std::vector<double> > features;
+    std::vector< std::vector<double> > labels;
+public:
+    bool load(std::string infile);
+    bool save(std::string outfile);
+
+    int getN_i() { return N_i; }
+    int getN_o() { return N_o; }
+    int getN_s() { return N_s; }
+    int getFeature(int s, int i){ return features[s][i]; }
+    int getLabel(int s, int i){ return labels[s][i]; }
+
+};
+
 class NeuralNet {
-	// count of nodes in each layer (input,hidden,output)
-	int N_i, N_h, N_o;
-	// store weights in a 3D vector (layers,i,j)
-	std::vector< std::vector< std::vector<double> > > weights;
+    // count of nodes in each layer (input,hidden,output)
+    int N_i, N_h, N_o;
+    // store weights in a 3D vector (layers,i,j)
+    std::vector< std::vector< std::vector<double> > > weights;
+    //std::vector< std::vector< std::vector<double> > > activations;
 
 public:
-	bool load(std::string infile);
-	bool save(std::string outfile);
+    bool load(std::string infile);
+    bool save(std::string outfile);
+    void train(Dataset &data, double learnRate, int numEpochs);
 };
+
